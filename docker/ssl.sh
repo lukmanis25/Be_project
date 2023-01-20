692013b4db33e12357cf28c658dfef0e100f3572
Hemdallv <sdemsw@outlook.com>
2023-01-20 18:35:35 +0100

    Update ssl.sh

--- docker/ssl.sh
+++ docker/ssl.sh
@@ -3,8 +3,7 @@ cd ssl
 touch apache-selfsigned.key
 touch apache-selfsigned.crt
 cd ..
-rm -rf /etc/apache2/sites-available/000-default.conf
-cp /ssl/000-default.conf /etc/apache2/sites-available/000-default.conf
+
 cp /ssl/apache.key /etc/ssl/private/apache.key
 cp /ssl/apache-certificate.crt /etc/ssl/certs/apache-certificate.crt
 
