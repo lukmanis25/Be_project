d7764e80de117670e6e2a07840fa1428aaddbf6c
Hemdallv <sdemsw@outlook.com>
2023-01-20 18:37:25 +0100

    Update ssl.sh

--- docker/ssl.sh
+++ docker/ssl.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd ssl
-touch apache-selfsigned.key
-touch apache-selfsigned.crt
-cd ..
 
 cp /ssl/apache.key /etc/ssl/private/apache.key
 cp /ssl/apache-certificate.crt /etc/ssl/certs/apache-certificate.crt
