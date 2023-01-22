59e55682d95985deb11401d8398a01c35d716b71
Hemdallv <sdemsw@outlook.com>
2023-01-22 14:14:01 +0100

    Delete ssl.sh

--- docker/ssl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp /ssl/apache.key /etc/ssl/private/apache.key
-cp /ssl/apache-certificate.crt /etc/ssl/certs/apache-certificate.crt
-
-a2enmod ssl
-service apache2 restart
-
-apachectl -D FOREGROUND
